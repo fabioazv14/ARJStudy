cfe22b782af58cde3d5634642088685f68665e8e
sk4iripa <fabio.azv14@gmail.com>
2025-07-15 12:13:12 +0100

    feat: enhanced design of card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -13,7 +13,7 @@ interface CardProps {
 
 export default function Card({ imageSrc, imageAlt, title, linkHref, linkText }: CardProps) {
   return (
-    <div className="max-w-sm w-70 rounded-4xl overflow-hidden shadow-xl bg-gradient-to-b from-red-400 to-orange-300 flex flex-col">
+    <div className="max-w-sm w-70 rounded-4xl overflow-hidden shadow-xl bg-gradient-to-b from-red-400 to-gray-100 flex flex-col">
         <div className={`${bebasNeue.variable} px-6 py-4 flex-grow mx-auto`}>
             <h2 className="font-bold text-4xl text-[var(--white)] mb-2 font-title">{title}</h2>
         </div>
@@ -23,7 +23,7 @@ export default function Card({ imageSrc, imageAlt, title, linkHref, linkText }: 
             alt={imageAlt}
         />
         <div className="px-6 pb-4 mx-auto">
-            <Link href={linkHref} className="w-50 h-12 inline-block bg-blue-400 text-white px-4 py-2 rounded-2xl hover:bg-blue-500 transition text-lg text-center">{linkText}</Link>
+            <Link href={linkHref} className="w-50 h-12 inline-block bg-red-400 text-white px-4 py-2 rounded-2xl hover:bg-red-500 transition text-lg text-center">{linkText}</Link>
         </div>
     </div>
   );
