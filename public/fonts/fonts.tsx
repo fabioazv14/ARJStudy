4350d2dd482fed6a1c033941e2d802bd2d5cc2a7
Fábio Santos Azevedo <fabio.azv14@gmail.com>
2026-06-28 22:16:14 +0100

    feat: add fonts file

--- public/fonts/fonts.tsx
+++ public/fonts/fonts.tsx
@@ -1,13 +1,7 @@
-import { Bebas_Neue, Fjalla_One } from "next/font/google"
+export const fjallaOne = {
+  variable: "font-navbut",
+};
 
-export const fjallaOne = Fjalla_One({
-    subsets: ["latin"],
-    weight: "400",
-    variable: "--font-fjalla-one",
-});
-
-export const bebasNeue = Bebas_Neue({
-    subsets: ["latin"],
-    weight: "400",
-    variable: "--font-bebas-neue"
-})
+export const bebasNeue = {
+  variable: "font-title",
+};
